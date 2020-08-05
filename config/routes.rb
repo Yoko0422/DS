6bb15f17135fa5afcc1639d5fc1ae4581895200e
Yoko0422 <cerise2@echecs.sakura.ne.jp>
2020-08-05 08:38:10 +0000

    ユーザー登録画面へのルート削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   
   resources :users
   
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+   delete '/logout', to: 'sessions#destroy'
   
 end
