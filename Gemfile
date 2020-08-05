a00324b82f9bc621e329d6b7a0619914fa1616ef
Yoko0422 <cerise2@echecs.sakura.ne.jp>
2020-08-05 06:53:48 +0000

    完成

--- Gemfile
+++ Gemfile
@@ -75,3 +75,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'jquery-rails'
 gem 'bcrypt'
+gem 'carrierwave'
--- app/models/main.rb
+++ app/models/main.rb
@@ -1,2 +1,11 @@
 class Main < ApplicationRecord
+  mount_uploader :file1, FileUploader
+  mount_uploader :file2, FileUploader
+  mount_uploader :file3, FileUploader
+  mount_uploader :file4, FileUploader
+   
+  validates :title, presence: true
+  validates :name, presence: true
+  validates :text, presence: true
+  
 end
